8b527f933e230cc840ab0fc5716ad6fbc115b2f9
Anthony Grimaldo Ramos <hernangrim.10@gmail.com>
2024-08-07 19:41:29 +0200

    iniciando ex02

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agrimald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 18:58:35 by agrimald          #+#    #+#             */
+/*   Updated: 2024/08/07 19:10:04 by agrimald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+
+std::string mensajito(std::string msg);
+int main();
+
+# endif
